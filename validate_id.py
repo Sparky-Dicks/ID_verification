7980dcbaa55e19f9cca392b5c16ed6bb656475be
Mark Dicks <117034609+Sparky-Dicks@users.noreply.github.com>
2023-01-16 17:09:03 +0200

    Update validate_id.py
    Updating code for editing

--- validate_id.py
+++ validate_id.py
@@ -1,6 +1,7 @@
 def get_input():
     try:
         id_number = int(input("Enter your ID number: "))
+        # print(id_number)
         return validate_id_main(id_number)
     except ValueError:
         print("Must not contain letters or any special characters")
@@ -20,37 +21,47 @@ def get_number(list_numbers, str_id):
         if i%2!=0:
             total = int(list_numbers[i])*2
             # print("Total:", total)
+            # print(list_numbers[i])
             if total > 9:
+                print("Too big: ", total)
                 total = (total//10) + (total%10)
             list_numbers[i] = total
+            print("Index: ", list_numbers[i])
             tester += int(list_numbers[i])
         else:
-            tester += int(list_numbers[i])
+            # tester += int(list_numbers[i])
             list_numbers[i] = int(list_numbers[i])
         # print("The List:", list_numbers)
         i+=1
     new_int = int(convert_list(list_numbers))//10
-    print(tester)
+    print("Tester: ", tester)
     return tester
 
 
 def validate_id_math(id_number, str_id):
     final_answer = (id_number%10)
+    print(final_answer)
     check_this_num = get_number(id_number, str_id)
     print(((check_this_num*9)%10),final_answer)
-    if ((check_this_num*9)%10) != final_answer:
+    if ((check_this_num*9)%10) == final_answer:
+        # print("***")
         return True
     else:
+        # print("**")
         return False
 
 
 def validate_id_main(id_num):
     id_num_str = str(id_num)
-    if len(id_num_str) != 13 or id_num_str[12]=="0" or validate_id_math(id_num, id_num_str):
+    while len(id_num_str) != 13:
+        print("Check", id_num_str)
+        id_num_str = "0" + str(id_num_str)
+    print(id_num_str, "***")
+    if id_num_str[12]=="0" or validate_id_math(id_num, id_num_str):
         print("PLease enter a valid ID number")
         return get_input()
     else:
-        return id_num
+        return id_num_str
 
 
 if __name__ == "__main__":
